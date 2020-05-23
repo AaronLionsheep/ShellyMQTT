1b5c5466843ab6685b14ed3704ebfbd16fb7284f
Aaron Rohrbaugh <aaronr@vt.edu>
2020-05-23 12:13:36 -0400

    #68 - Added validation and tests the RGBW2 devices.

--- ShellyMQTT.indigoPlugin/Contents/Server Plugin/Devices/RGBW2/Shelly_RGBW2_White.py
+++ ShellyMQTT.indigoPlugin/Contents/Server Plugin/Devices/RGBW2/Shelly_RGBW2_White.py
@@ -171,3 +171,16 @@ class Shelly_RGBW2_White(Shelly_1PM):
             self.device.updateStateImageOnServer(indigo.kStateImageSel.DimmerOn)
         else:
             self.device.updateStateImageOnServer(indigo.kStateImageSel.DimmerOff)
+
+    @staticmethod
+    def validateConfigUI(valuesDict, typeId, devId):
+        """
+        Validates a device config.
+
+        :param valuesDict: The values in the Config UI.
+        :param typeId: the device type as specified in the type attribute.
+        :param devId: The id of the device (0 if a new device).
+        :return: Tuple of the form (valid, valuesDict, errors)
+        """
+
+        return Shelly_1PM.validateConfigUI(valuesDict, typeId, devId)
