8bf0f7e07d85b58f9aedf117ffab11f6313ed301
Aaron <aaronr@vt.edu>
2021-03-01 18:29:56 -0500

    Moved menu generator methods for #104

--- ShellyMQTT.indigoPlugin/Contents/Server Plugin/plugin.py
+++ ShellyMQTT.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -45,6 +45,7 @@ from Devices.Addons.Shelly_Addon_Detached_Switch import Shelly_Addon_Detached_Sw
 from Queue import Queue
 import logging
 import callbacks
+import menu_generators
 
 kCurDevVersion = 0  # current version of plugin devices
 
@@ -762,170 +763,6 @@ class Plugin(indigo.PluginBase):
             # store the announcement within the broker list using the id as the key
             self.discoveredDevices[brokerId][identifier] = announcement
 
-    ###############################
-    #     Getters for Devices     #
-    ###############################
-
-    def getBrokerDevices(self, filter="", valuesDict=None, typeId="", targetId=0):
-        """
-        Gets a list of available broker devices.
-
-        :param filter:
-        :param valuesDict:
-        :param typeId:
-        :param targetId:
-        :return: A list of brokers.
-        """
-
-        brokers = []
-        for dev in indigo.devices.iter():
-            if dev.protocol == indigo.kProtocol.Plugin and \
-                    dev.pluginId == "com.flyingdiver.indigoplugin.mqtt" and \
-                    dev.deviceTypeId != 'aggregator':
-                brokers.append((dev.id, dev.name))
-
-        brokers.sort(key=lambda tup: tup[1])
-        return brokers
-
-    def getShellyDevices(self, filter="", valuesDict=None, typeId="", targetId=0):
-        """
-        Gets a list of available Shelly devices.
-
-        :return: A list of shelly device tuples of the form (deviceId, name).
-        """
-
-        shellies = []
-        if filter:
-            types = [cat.strip() for cat in filter.split(",")]
-            for dev_type in types:
-                for dev in indigo.devices.iter(dev_type):
-                    shellies.append((dev.id, dev.name))
-        else:
-            for dev in indigo.devices.iter("self"):
-                shellies.append((dev.id, dev.name))
-
-        shellies.sort(key=lambda d: d[1])
-        return shellies
-
-    def getDiscoveredDevices(self, filter=None, valuesDict={}, typeId=None, targetId=None):
-        """
-        Gets a list of discovered devices
-
-        :return: A list of device tuples of the form (<broker-id>|<identifier>, displayName)
-        """
-
-        devices = []
-        for brokerId in self.discoveredDevices:
-            brokerName = indigo.devices[brokerId].name
-            brokerDevices = self.discoveredDevices[brokerId]
-            for identifier in brokerDevices:
-                devices.append((u"{}|{}".format(brokerId, identifier), u"{} on {}".format(identifier, brokerName)))
-
-        if len(devices) == 0:
-            # No devices found, add a disabled option to indicate this
-            devices.append((-1, u"%%disabled:No discovered devices%%"))
-
-        return devices
-
-    def getTemplateDevices(self, filter=None, valuesDict={}, typeId=None, targetId=0):
-        """
-        Builds a list of devices that have been discovered or that are related to the device being created.
-
-        :param filter: A comma separated list of device types
-        :return: A list of device tuples of the form (<broker-id>|<identifier>, displayName)
-        """
-
-        # Build the discovered devices section
-        discovered_devices = []
-        for brokerId in self.discoveredDevices:
-            brokerName = indigo.devices[brokerId].name
-            brokerDevices = self.discoveredDevices[brokerId]
-            for identifier in brokerDevices:
-                discovered_devices.append((u"{}|shellies/{}".format(brokerId, identifier), u"{} on {}".format(identifier, brokerName)))
-
-        # Build the related devices section
-        related_devices = []
-        types = [cat.strip() for cat in filter.split(",")]
-        for dev_type in types:
-            for device in indigo.devices.iter(dev_type):
-                if device.id != targetId:  # We don't want the current device to be used as a template for itself
-                    brokerId = device.pluginProps.get('broker-id', None)
-                    address = device.pluginProps.get('address', None)
-                    message_type = device.pluginProps.get('message-type', None)
-                    related_devices.append((u"{}|{}|{}|{}".format(brokerId, address, message_type, device.name), u"{}".format(device.name)))
-
-        # Join the device lists and build the menu
-        devices = []
-        devices.append((u"-1", u"%%disabled:Discovered Devices:%%"))
-        if len(discovered_devices) == 0:
-            devices.append((u"-1", u"%%disabled:No Discovered Devices%%"))
-        else:
-            devices.extend(discovered_devices)
-
-        devices.append((u"-1", u"%%separator%%"))
-        devices.append((u"-1", u"%%disabled:Related Devices:%%"))
-        if len(related_devices) == 0:
-            devices.append((u"-1", u"%%disabled:No Related Devices%%"))
-        else:
-            devices.extend(related_devices)
-
-        return devices
-
-    def getUpdatableShellyDevices(self, filter=None, valuesDict={}, typeId=None, targetId=None):
-        """
-        Gets a list of shelly devices that can be updated.
-
-        :return: A list of Indigo deviceId's corresponding to updatable shelly devices.
-        """
-
-        shellies = self.getShellyDevices()
-        updatable = []
-        for dev in shellies:
-            device = indigo.devices[dev[0]]
-            if device.states.get('has-firmware-update', False):
-                updatable.append(dev)
-        return updatable
-
-    def getAddonHostDevices(self, filter=None, valuesDict={}, typeId=None, targetId=None):
-        """
-        Gets a list of Shelly devices (1/1PM) that are capable of "hosting" an add-on sensor.
-
-        :return: A list of devices which are capable to hosting add-ons.
-        """
-
-        hostable_model_categories = {  # Shelly models that can host an "addon"
-            "ds1820": ["shelly-1", "shelly-1pm", "shelly-uni-relay", "shelly-uni-input"],
-            "dht22": ["shelly-1", "shelly-1pm", "shelly-uni-relay", "shelly-uni-input"],
-            "detached-switch": ["shelly-1", "shelly-1pm", "shelly-2-5-relay", "shelly-4-pro", "shelly-dimmer-sl"]
-        }
-
-        hostable_models = set()
-        if filter:
-            categories = [cat.strip() for cat in filter.split(",")]
-            for category in categories:
-                models = hostable_model_categories.get(category, None)
-                if models:
-                    for model in models:
-                        hostable_models.add(model)
-
-        shellies = self.getShellyDevices()
-        hostable = []
-        for dev in shellies:
-            shelly = self.shellyDevices.get(dev[0])
-            if shelly and shelly.device.deviceTypeId in hostable_models:
-                hostable.append(dev)
-        return hostable
-
-    #################################
-    #     Actions from the User     #
-    #################################
-
-
-
-    #####################
-    #     Utilities     #
-    #####################
-
     def printBrokerDeviceSubscriptions(self):
         """
         Prints the data structure that contains brokers, topics, and devices
@@ -1056,11 +893,22 @@ class Plugin(indigo.PluginBase):
             return False, valuesDict, errors
 
     def __getattr__(self, method_name):
+        """
+        Handles calling helper functions from other files that Indigo expects are in
+        this main plugin.py file
+
+        :param method_name: The name of the method trying to be called.
+        :return: A method to call
+        """
         def method(*args, **kwargs):
             if method_name.startswith("cb_"):
                 cb_method = method_name.replace("cb_", "")
                 injected_args = (self,) + args
                 return getattr(callbacks, cb_method)(*injected_args, **kwargs)
+            elif method_name.startswith("mg_"):
+                cb_method = method_name.replace("mg_", "")
+                injected_args = (self,) + args
+                return getattr(menu_generators, cb_method)(*injected_args, **kwargs)
             else:
                 return getattr(self, method_name)(*args, **kwargs)
         return method
